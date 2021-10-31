d4143d31e709b2a4f5508a5ec2d8c2587c336a71
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-31 20:14:26 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"COKK-UserBot ⚙️ V{BOT_VER} [乂 COKK-USERBOT SUDAH AKTIF COK! 乂]")
+    f"乂𝗖𝗼𝗸-𝗨𝘀𝗲𝗿𝗯𝗼𝘁乂 🔥 V7.0 [AKTIF COKKK😎!!!!]")
 
 
 if len(argv) not in (1, 3, 4):
