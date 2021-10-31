5658a8849bf5a59a00c6782aba576f1991395f72
ABKeceX <zulfhanmt@gmail.com>
2021-10-31 18:05:34 +0000

    pylint: auto fixes
    Signed-off-by: ABKeceX <zulfhanmt@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,6 +5,9 @@
 # Ported @MaafGausahSokap / JANGAN DI APUS BABI
 """Userbot initialization."""
 
+from userbot import (
+    ALIVE_NAME
+)
 import os
 import time
 import re
@@ -405,9 +408,6 @@ AFKREASON = None
 ZALG_LIST = {}
 
 # Import Userbot - Ported by RAMADHANI892
-from userbot import (
-    ALIVE_NAME
-)
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
