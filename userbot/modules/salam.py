f18809c0a87e6d023d48378ca72b93fd63542ece
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-01 10:53:57 +0800

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -1,5 +1,4 @@
 from platform import uname
-
 from userbot import ALIVE_NAME, CMD_HELP
 from userbot.events import register
 
@@ -7,55 +6,190 @@ from userbot.events import register
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
-
-@register(outgoing=True, pattern="^.p(?: |$)(.*)")
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**Assalamualaikum Dulu Biar Sopan😉**")
+    await typew.edit("𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...")
 
 
-@register(outgoing=True, pattern=r"^\.pe(?: |$)(.*)")
+@register(outgoing=True, pattern='^.atg(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**Assalamualaikum Warahmatullahi Wabarakatuh**")
+    await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇.... 𝐆𝐎𝐁𝐋𝐎𝐊𝐊𝐊𝐊𝐊!!!!")
 
 
-@register(outgoing=True, pattern="^.P(?: |$)(.*)")
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit(f"**Haii Salken Cok, Saya {DEFAULTUSER}**")
-    sleep(2)
-    await typew.edit("**Assalamualaikum...Cok? Pada Mati Ya**")
+    await typew.edit("𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...")
 
 
-@register(outgoing=True, pattern=r"^\.l(?: |$)(.*)")
+@register(outgoing=True, pattern='^.ast(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**Wa'alaikumsalam Cok**")
+    await typew.edit("𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......")
 
 
-CMD_HELP.update(
-    {
-        "salam": "**Plugin : **`salam`\
-        \n\n  •  **Syntax :** `.p`\
-        \n  •  **Function : **Assalamualaikum Dulu Biar Sopan😉..\
-        \n\n  •  **Syntax :** `.pe`\
-        \n  •  **Function : **salam Kenal dan salam\
-        \n\n  •  **Syntax :** `.l`\
-        \n  •  **Function : **Untuk Menjawab salam\
-        \n\n  •  **Syntax :** `.ass`\
-        \n  •  **Function : **Salam Bahas arab\
-        \n\n  •  **Syntax :** `.semangat`\
-        \n  •  **Function : **Memberikan Semangat.\
-        \n\n  •  **Syntax :** `.ywc`\
-        \n  •  **Function : **nMenampilkan Sama sama\
-        \n\n  •  **Syntax :** `.sayang`\
-        \n  •  **Function : **Kata I Love You.\
-        \n\n  •  **Syntax :** `.k`\
-        \n  •  **Function : **LU SEMUA NGENTOT COKK🔥😎\
-        \n\n  •  **Syntax :** `.j`\
-        \n  •  **Function : **NIMBRUNG GOBLOKK!!!🔥\
-    "
-    }
-)
+@register(outgoing=True, pattern='^K(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**𝐍𝐆𝐎𝐍𝐓𝐎𝐋𝐋𝐋𝐋𝐋𝐋**")
+
+
+@register(outgoing=True, pattern='^N(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**𝐍𝐆𝐄𝐍𝐓𝐎𝐎𝐎𝐎𝐎𝐎𝐎𝐓𝐓𝐓𝐓𝐓𝐓𝐓𝐓𝐓𝐓𝐓𝐓**")
+
+
+@register(outgoing=True, pattern='^B(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**𝐁𝐀𝐂𝐎𝐓 𝐃𝐀𝐇 𝐋𝐔, 𝐆𝐎𝐁𝐋𝐎𝐊!!!!**")
+
+
+@register(outgoing=True, pattern='^M(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**𝐁𝐇𝐀𝐀𝐀𝐊𝐒𝐒𝐒𝐒𝐒𝐒𝐒𝐒𝐒**")
+
+
+@register(outgoing=True, pattern='^Y(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**YAUDAH IYAAAAAA ASU**")
+
+
+@register(outgoing=True, pattern='^C(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MUKA LU HINA, GAUSAH SOK KERAS YA ANJENGG!!**")
+
+
+@register(outgoing=True, pattern='^S(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GAUSAH SOKAP DEH ANJING!!**")
+
+
+@register(outgoing=True, pattern='^V(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MACAM BAGUS AE LU BEGITU KONTOL!!**")
+
+
+@register(outgoing=True, pattern='^J(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**JELEK BANGET LU, NAJIS CUIHHHH!!!**")
+
+
+@register(outgoing=True, pattern='^A(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**PASANG PP DULU DEK,BIAR ORANG SEGRUP TAU BETAPA HINA NYA MUKA LU😆**")
+
+
+@register(outgoing=True, pattern='^X(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GC SAMPAH, CUIHHHHH!!!!!!!!!!!**")
+
+
+@register(outgoing=True, pattern='^Z(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
+
+
+@register(outgoing=True, pattern='^H(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Lawak Mulu ocehan lu,Ga ngena ke mental, CUIHHHH!!!**")
+
+
+@register(outgoing=True, pattern='^O(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**JANGAN MAEN BOT MULU, ALAY LU NGENTOTT,KESANNYA NORAK, CUIHHHH!!!**")
+
+
+@register(outgoing=True, pattern='^G(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Gak keren lu begitu tolol, kuburan bapak lu gw gali buat dijadiin kolam renang anak paud.Cuihhhhh!!!**")
+
+
+
+@register(outgoing=True, pattern='^I(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("𝐎𝐏𝐄𝐍 𝐕𝐂𝐒 𝐑𝐄𝐀𝐋"
+                     "\n\n"
+                     "𝙏𝙖𝙡𝙡𝙚𝙣𝙩 : @KasihKecil"
+                     "\n"
+                     "\n30 𝙠 - 5 𝙢𝙚𝙣𝙞𝙩"
+                     "\n50 𝙠 - 10 𝙢𝙚𝙣𝙞𝙩"
+                     "\n70 𝙠 - 15 𝙢𝙚𝙣𝙞𝙩"
+                     "\n100 𝙠 - 25𝙢𝙚𝙣𝙞𝙩"
+                     "\n"
+                     "\n𝐋𝐈𝐕𝐄 𝐒𝐇𝐎𝐖 𝐆𝐂"
+                     "\n"
+                     "\n150 𝙠 - 30 𝙢𝙚𝙣𝙞𝙩 ( 𝙎𝙇𝙊𝙒 )"
+                     "\n250 𝙠 - 30 𝙢𝙚𝙣𝙞𝙩 ( 𝘽𝘼𝙍-𝘽𝘼𝙍 )"
+                     "\n\n"
+                     "𝙏𝙛 𝙫𝙞𝙖 𝘿𝙖𝙣𝙖 💰"
+                     "\n- 𝙏𝙖𝙡𝙡𝙚𝙣𝙩 𝙂𝘾¹⁸⁺"
+                     "\n- 𝙏𝙞𝙙𝙖𝙠 𝙥𝙪𝙣𝙮𝙖 𝙜𝙧𝙪𝙗 𝙩𝙚𝙨𝙩𝙞"
+                     "\n- 𝙠𝙖𝙨𝙞𝙝 𝙘𝙪𝙢𝙖 𝙥𝙪𝙣𝙮𝙖 𝙨𝙖𝙩𝙪 𝙖𝙠𝙪𝙣 _𝙉𝙊 𝙁𝘼𝘾𝙀 _ 📍𝙮𝙖𝙣𝙜 𝙧𝙚𝙖𝙡 @KasihKecil 𝙮𝙖𝙣𝙜 𝙡𝙖𝙞𝙣 𝙗𝙪𝙠𝙖𝙣 𝙖𝙠𝙪 ."
+                     "\n\n"
+                     "𝐀𝐃𝐌𝐈𝐍 :"
+                     "\n@DENSUSS_86 @NOTHINGqqqq")
+
+                    
+
+CMD_HELP.update({
+    "salam":
+    "P\
+\nUsage: Untuk Memberi salam.\
+\n\nL\
+\nUsage: Untuk Menjawab Salam.\
+\n\nK\
+\nUsage: Untuk mengontoli mereka.\
+\n\nN\
+\nUsage: Kalo kesel coba aja.\
+\n\nB\
+\nUsage: Buat Ngatain Yang Suka Bacot.\
+\n\nM\
+\nUsage: Tersedak meledek.\
+\n\nY\
+\nUsage: Buat yang males adu bacot.\
+\n\nC\
+\nUsage: Buat menghujat.\
+\n\nS\
+\nUsage: Haha sokap."
+})
+
+CMD_HELP.update({
+    "salam2":
+    "V\
+\nUsage: Hujat Orang caper.\
+\n\nJ\
+\nUsage: Hujat Jamet.\
+\n\nA\
+\nUsage: Hujat yang gapunya muka.\
+\n\nX\
+\nUsage: Ngatain Grup wkwk.\
+\n\nZ\
+\nUsage: teruntuk petarung.\
+\n\nH\
+\nUsage: Coba dewek ah.\
+\n\n.atg\
+\nUsage: Istighfar 1.\
+\n\n.ast\
+\nUsage: Istighfar 2.\
+\n\nO\
+\nUsage: Ngatain org norak.\
+\n\nG\
+\nUsage: Liat Sendiri."
+})
